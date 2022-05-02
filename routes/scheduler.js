cd8c16cc7adc99f2c136123793d75697b68b85b1
japnit01 <japnit2012@gmail.com>
2022-05-03 01:58:50 +0530

    registered events send to frontend

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -6,21 +6,32 @@ const Fest = require('../models/fests');
 let Events = require('../models/events');
 
 const createjson = async (userSchedule) => {
-    // let festjson = [];
-    // let eventjson = [];
-    let newjson = []
+    
+    let contentjson = []
+    let registeredEvents = [];
     for (let i = 0; i < userSchedule.length; i++) {
-        const fest = await Fest.findOne({ _id: userSchedule[i].fest_id },{coordinators:0,_id:0,user:0,timestamp:0});
+        const fest = await Fest.findOne({ _id: userSchedule[i].fest_id },{coordinators:0,user:0,timestamp:0});
         let eventids = userSchedule[i].events.map(event => event.event_id)
-        // console.log('eventids: ',eventids)
+
+        let registeredeventids = [];
+        userSchedule[i].events.map(event => {
+            if(event.isRegistered === true) {
+                registeredeventids.push(event.event_id);
+            }
+        })
         
-        const event = await Events.find({_id:{$in: eventids}},{_id:0,fest_id:0}).catch(err => {
+        const event = await Events.find({_id:{$in: eventids}},{fest_id:0}).catch(err => {
             return res.status(400).send('not able to fetch the events with these ids')
         });
-        newjson.push({fest,event});
+
+        const registered = await Events.find({ _id: {$in: registeredeventids} }).catch(err => {
+            return res.status(400).send('not able to fetch the registered events with these ids')
+        })
+        contentjson.push({fest,event});
+        registeredEvents.push({fest,event: registered})
     }
-    // console.log('newjson: ',newjson)
-    return newjson;
+    console.log('contentjson: ',contentjson)
+    return {contentjson, registeredEvents};
 }
 
 router.get('/getSchedule', validateUser, async (req, res,) => {
@@ -29,10 +40,14 @@ router.get('/getSchedule', validateUser, async (req, res,) => {
         return res.status(500).json({ error: err });
     });
 
+    // let registeredEvents = await Scheduler.find({user_id: req.user, 'events.isRegistered': true},{name:1,organisation: 1,type:1,fee:1}).catch(err => {
+    //     return res.status(400).send('not able to fetch your registered events');
+    // })
+
     // console.log(userSchedule)
-    const contentjson = await createjson(userSchedule);
-    console.log('contentjson: ',contentjson);
-    res.status(200).send(contentjson);
+    const {contentjson,registeredEvents} = await createjson(userSchedule);
+    // console.log('contentjson: ',contentjson);
+    res.status(200).json({contentjson, registeredEvents});
 });
 
 router.post('/addToSchedule/:festid/:eventid', validateUser, async (req, res) => {
@@ -60,13 +75,17 @@ router.post('/addToSchedule/:festid/:eventid', validateUser, async (req, res) =>
     res.status(200).json({ "Added to schedule": event });
 });
 
-router.delete('/deleteFromSchedule/:eventid', validateUser, async (req, res) => {
+router.delete('/deleteFromSchedule/:festid/:eventid', validateUser, async (req, res) => {
 
-    let event = await Scheduler.updateOne({ user_id: req.user }, { $pull: { 'events.event_id': req.params.eventid } }).catch(err => {
+    let event = await Scheduler.updateOne({ user_id: req.user, fest_id: req.params.festid}, { $pull: { 'events.event_id': req.params.eventid }}).catch(err => {
         return res.status(500).json({ error: 'Unable to register for the event at the moment' });
     });
+    // let event = await Scheduler.updateOne({ user_id: req.user }, { $pull: { events: { event_id: req.params.eventid }} }).catch(err => {
+    //     // console.log(req.params.eventid)
+    //     return res.status(500).json({ error: 'Unable to unregister for the event at the moment' });
+    // });
 
-    res.status(200).json({ 'event deleted': event });
+    res.status(200).send(event)
 });
 
 router.post('/register-event/:festid/:eventid', validateUser, async (req, res) => {
@@ -100,8 +119,6 @@ router.post('/unregister-event/:eventid', validateUser, async (req, res) => {
     let record = await Scheduler.updateOne({ user_id: req.user }, { $pull: { events: { event_id: req.params.eventid } } }).catch(err => {
         return res.status(500).json({ error: 'Unable to register for the event at the moment' });
     });
-
-
 
     res.status(200).json({ 'Updated Registered Events': record });
 });
