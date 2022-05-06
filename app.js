ec131488755f6abc37620c2d51c8f7bd0c328e19
japnit01 <japnit2012@gmail.com>
2022-05-07 03:22:51 +0530

    secret updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 connecttomongo();
 
 const app = express()
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth',require('./routes/auth'))
