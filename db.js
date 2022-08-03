5ae1ee830c154f9d8f28eeb04d6fb0eebdf5b230
Sarthak Arora <sarthakarora1503@gmail.com>
2022-08-03 12:25:42 +0530

    db.js changed

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let dbUrl = "mongodb://localhost/festdb";//process.env.DB_URL ;
+let dbUrl = "mongodb://localhost/festdb" || process.env.DB_URL ;
 
 const connecttomongo = async() =>{
     mongoose.connect(dbUrl,{useNewUrlParser: true,useUnifiedTopology:true});
